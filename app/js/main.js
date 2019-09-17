7fafb62507313aaeb26c63188a4aecc362c07f99
Dimas <zhuravel16@gmail.com>
2019-09-17 19:33:49 +0300

    finish desctop_version

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@ $(function(){
 
 
 
-    $('.slider_inner').slick({
+    $('.slider_inner, .news_slider-inner').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         infinite: false
